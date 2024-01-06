38a82f823bc32db1e1a7be746f1242d0c8e761de
Igor <Igor@mail.ru>
2024-01-06 13:13:57 +0600

    Commit3

--- app/src/main/java/com/example/electronicmagazine/MainActivity.kt
+++ app/src/main/java/com/example/electronicmagazine/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.electronicmagazine
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this@MainActivity, MainActivity2::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, 5000)
     }
 }
